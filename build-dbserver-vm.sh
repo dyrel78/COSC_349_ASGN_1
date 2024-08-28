3397bd6e7d197fb11833113364a1b1322e15b17c
lumdy690 <lumdy690@student.otago.ac.nz>
2024-08-29 10:00:49 +1200

    Changed some default credentials to my own

--- build-dbserver-vm.sh
+++ build-dbserver-vm.sh
@@ -7,7 +7,7 @@
 apt-get update
       
 # We create a shell variable MYSQL_PWD that contains the MySQL root password
-export MYSQL_PWD='insecure_mysqlroot_pw'
+export MYSQL_PWD='root_password'
 
 # If you run the `apt-get install mysql-server` command
 # manually, it will prompt you to enter a MySQL root
@@ -28,24 +28,24 @@ service mysql start
 
 # Run some setup commands to get the database ready to use.
 # First create a database.
-echo "CREATE DATABASE fvision;" | mysql
+echo "CREATE DATABASE drinksDB;" | mysql
 
 # Then create a database user "webuser" with the given password.
-echo "CREATE USER 'webuser'@'%' IDENTIFIED BY 'insecure_db_pw';" | mysql
+echo "CREATE USER 'webuser'@'%' IDENTIFIED BY 'password';" | mysql
 
 # Grant all permissions to the database user "webuser" regarding
 # the "fvision" database that we just created, above.
-echo "GRANT ALL PRIVILEGES ON fvision.* TO 'webuser'@'%'" | mysql
+echo "GRANT ALL PRIVILEGES ON drinksDB.* TO 'webuser'@'%'" | mysql
 
 # Set the MYSQL_PWD shell variable that the mysql command will
 # try to use as the database password ...
-export MYSQL_PWD='insecure_db_pw'
+export MYSQL_PWD='password'
 
 # ... and run all of the SQL within the setup-database.sql file,
 # which is part of the repository containing this Vagrantfile, so you
 # can look at the file on your host. The mysql command specifies both
 # the user to connect as (webuser) and the database to use (fvision).
-cat /vagrant/setup-database.sql | mysql -u webuser fvision
+cat /vagrant/setup-database.sql | mysql -u webuser drinksDB
 
 # By default, MySQL only listens for local network requests,
 # i.e., that originate from within the dbserver VM. We need to
