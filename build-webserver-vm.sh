cf3647e4af3d53237581db9a53e4ce530c353e45
lumdy690 <lumdy690@student.otago.ac.nz>
2024-08-31 16:29:45 +1200

    updated .conf file for non admin(voter) user,  changed root files to be in /voter folder

--- build-webserver-vm.sh
+++ build-webserver-vm.sh
@@ -9,10 +9,10 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
             
 # Change VM's webserver's configuration to use shared folder.
 # (Look inside test-website.conf for specifics.)
-cp /vagrant/test-website.conf /etc/apache2/sites-available/
+cp /vagrant/user-website.conf /etc/apache2/sites-available/
 
 # activate our website configuration ...
-a2ensite test-website
+a2ensite user-website
 # ... and disable the default website provided with Apache
 a2dissite 000-default
 # Restart the webserver, to pick up our configuration changes
